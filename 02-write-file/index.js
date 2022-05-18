57bfd6f6906cb8a681e1685e180e353fcf783eca
Eremor <artem.s13@gmail.com>
2022-05-18 15:53:53 +0300

    feat: add solution task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(pathToFile);
+
+process.stdout.write('Please enter text:\n');
+
+const rl = readline.createInterface({ input: process.stdin });
+
+const exitConsole = () => {
+  process.stdout.write('Bye! Have a nice day');
+  process.exit(0);
+}
+
+rl.on('line', (line) => {
+  if(line === 'exit') {
+    exitConsole();
+  } else {
+    writeStream.write(`${line}\n`);
+  }
+})
+
+process.on('SIGINT', () => {
+  exitConsole();
+})
